c84d92d15346211b77beb1fbc2ecd81be609e273
Wim Wiltenburg <wim.wiltenburg@mendesgans.nl>
2022-02-09 09:08:38 +0100

    demo starter

--- src/components/StockList.vue
+++ src/components/StockList.vue
@@ -1,95 +1,65 @@
 <template>
-  <div>
-    <table class="table">
-      <thead class="table-striped" >
-        <tr>
-          <th>Stock name</th>
-          <th>Price</th>
-          <th>Previous Price</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr v-for="stock in stocks" :key="stock.id">
-        <td>{{ stock.name }}</td>
-        <td
-          :class="{
-            down: stock.price < stock.previousPrice,
-            up: stock.price > stock.previousPrice,
-            same: stock.price == stock.previousPrice
-          }"
-        >
-          {{ stock.currency }} {{ stock.price }}
-        </td>
-        <td>{{ stock.previousPrice }}</td>
-      </tr>
-      </tbody>
-    </table>
-    <button @click="updatePrice" class="btn btn-primary">Update</button>
+  <h1>Stock list</h1>
+  <div class="row">
+    <!-- Individual stocks -->
+    <div class="col-md-4 mt-4" v-for="stock in stocks" :key="stock.name">
+      <div class="card">
+        <div class="card-header">{{ stock.name }}</div>
+        <div class="card-body">
+          <span
+            :class="{
+              down: stock.price < stock.previousPrice,
+              up: stock.price > stock.previousPrice,
+            }"
+            >{{ stock.currency }} {{ stock.price.toFixed(4) }}</span
+          >
+        </div>
+      </div>
+    </div>
+    <!-- End of individual stocks -->
   </div>
 </template>
-        
-    
+
 <script>
 export default {
   name: "StockList",
   data() {
     return {
       stocks: [
-        {
-          id: 1,
-          name: "BMW",
-          price: 61.05,
-          previousPrice: 0,
-          currency: "EUR",
-        },
-        {
-          id: 2,
-          name: "Caterpillar",
-          price: 146.49,
-          previousPrice: 0,
-          currency: "USD",
-        },
-        {
-          id: 3,
-          name: "AMD",
-          price: 76.5,
-          previousPrice: 0,
-          currency: "USD",
-        },
-        {
-          id: 4,
-          name: "Gazprom",
-          price: 4.583,
-          previousPrice: 0,
-          currency: "USD",
-        },
+        { name: "BMW", price: 61.05, previousPrice: 0, currency: "€" },
+        { name: "Caterpillar", price: 146.49, previousPrice: 0, currency: "$" },
+        { name: "AMD", price: 76.5, previousPrice: 0, currency: "$" },
+        { name: "Gazprom", price: 4.583, previousPrice: 0, currency: "$" },
       ],
+      portfolio: [],
     };
   },
   methods: {
-    updatePrice() {
+    updatePrices() {
       this.stocks.forEach((stock) => {
         stock.previousPrice = stock.price;
         stock.price += (Math.random() - 0.5) * 2;
-        if (stock.price < 0) stock.price = 0;
+        if (stock.price < 0) {
+          stock.price = 0;
+        }
       });
     },
-  },
+  },  
   mounted() {
-    setInterval(() => this.updatePrice(), 1000);
+    setInterval(() => {
+      this.updatePrices();
+    }, 1000);
   },
 };
 </script>
 
-<style>
+<style scoped>
 .up {
   color: green;
+  font-size: 50px;
 }
-
 .down {
   color: red;
-}
-.same {
-  color: black;
+  font-size: 50px;
 }
 </style>
